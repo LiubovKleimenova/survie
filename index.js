c2014ed7a2963ac427f15a3d8e3c07a7e62fed41
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-05-11 00:02:25 -0400

    started oauth

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth-20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-const PORT = process.env.PORT || 5000
+passport.use(
+	new GoogleStrategy({
+		clientID: keys.googleClientID,
+		clientSecret: keys.googleClientSecret,
+	})
+);
 
-app.get("/", (req, res) => {
-  res.send({"bye": "there"})
-})
-
-app.listen(PORT)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
