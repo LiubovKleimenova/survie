8ee913357eedc3247f519b998adfb23e178d1166
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-05-12 16:52:14 -0400

    fb auth w/passport

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,7 +8,23 @@ module.exports = (app) => {
 		})
 	);
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+	app.get("/auth/google/callback", passport.authenticate("google"));
+
+	app.get(
+		"/auth/facebook",
+		passport.authenticate("facebook", {
+			scope: ["id", "email"],
+		})
+	);
+
+  app.get(
+		"/auth/facebook/callback",
+		passport.authenticate("facebook", { failureRedirect: "/login" }),
+		function (req, res) {
+			// Successful authentication, redirect home.
+			res.redirect("/");
+		}
+	);
   
   app.get("/api/current_user", (req, res) => {
     res.send(req.user);
--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
@@ -32,6 +33,32 @@ passport.use(
 					});
 				}
 			});
+		}
+	)
+);
+
+passport.use(
+	new FacebookStrategy(
+		{
+			clientID: keys.facebookClientID,
+			clientSecret: keys.facebookClientSecret,
+			callbackURL: "/auth/facebook/callback",
+			profileFields: ["id", "email"]
+		},
+		(accessToken, refreshToken, profile, done) => {
+			console.log(accessToken);
+			console.log(refreshToken);
+			console.log(profile);
+			
+			// User.findOne({ googleId: profile.id }).then((existingUser) => {
+			// 	if (existingUser) {
+			// 		done(null, existingUser);
+			// 	} else {
+			// 		new User({ googleId: profile.id }).save().then((user) => {
+			// 			done(null, user);
+			// 		});
+			// 	}
+			// });
 		}
 	)
 );
