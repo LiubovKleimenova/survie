1366d2fd4e53cd75853af9207a3491acff769564
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-05-12 21:41:55 -0400

    add deplyment logic

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,5 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
@@ -37,28 +36,3 @@ passport.use(
 	)
 );
 
-passport.use(
-	new FacebookStrategy(
-		{
-			clientID: keys.facebookClientID,
-			clientSecret: keys.facebookClientSecret,
-			callbackURL: "/auth/facebook/callback",
-			profileFields: ["id", "email"]
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log(accessToken);
-			console.log(refreshToken);
-			console.log(profile);
-			
-			// User.findOne({ googleId: profile.id }).then((existingUser) => {
-			// 	if (existingUser) {
-			// 		done(null, existingUser);
-			// 	} else {
-			// 		new User({ googleId: profile.id }).save().then((user) => {
-			// 			done(null, user);
-			// 		});
-			// 	}
-			// });
-		}
-	)
-);
