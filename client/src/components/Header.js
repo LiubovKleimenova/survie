7b6615ed5da78f8eb0a8cdbed3322c01c6e4ee8b
LiubovKleimenova <kleimenovaliubov@gmail.com>
2020-05-18 22:23:29 -0400

    Header component w/materialize css

---
+++ client/src/components/Header.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+
+class Header extends Component {
+  render() {
+    return (
+			<nav>
+				<div className="nav-wrapper">
+					<a className="left brand-logo">
+						Survie
+					</a>
+
+					<ul id="nav-mobile" className="right">
+						<li>
+							<a href="/auth/google">Login with Google</a>
+						</li>
+						
+					</ul>
+				</div>
+			</nav>
+		);
+  }
+}
+
+export default Header;
